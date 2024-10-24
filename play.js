6dd1ca0be657357273a04d7ae803ed5269ac6ca5
Jaidenpearson <[jaidensrpearson@gmail.com]>
2024-10-23 20:11:21 -0700

    connected to snek

---
+++ play.js
@@ -0,0 +1,24 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "172.27.185.245",
+    port: "50541",
+
+  });
+
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on('connect', (data) => {
+    console.log(data)
+  })
+
+  return conn;
+  
+};
+
+console.log("Connecting ...");
+connect();
